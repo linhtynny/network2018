96d4996e83f05f555744ae09e3e33a4790d84e7a
Linh Duong <linhtynny@Linhs-MacBook-Pro.local>
2018-03-24 11:49:14 +0700

    Multiplexed Nonblocking Server

---
+++ PracticalWork9/client.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <unistd.h> 
+#include <sys/socket.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/select.h>
+
+int main(){
+
+struct sockaddr_in saddr;
+struct hostent *h;
+int sockfd;
+unsigned short port = 8785;
+char str[80], buffer[100];
+
+if ((sockfd=socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    printf("Error creating socket\n");
+}
+else{
+    printf("Socket! \n");
+}
+if ((h=gethostbyname("localhost")) == NULL) { 
+	printf("Unknown host \n");
+	printf("%s\n", "Enter hostname:");
+	scanf("%s", str);
+	h = gethostbyname(str);
+}
+
+setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &(int){ 1 }, sizeof(int));
+
+// int fl = fcntl(sockfd, F_GETFL, 0);
+//       fl |= O_NONBLOCK;
+//       fcntl(sockfd, F_SETFL, fl);
+
+
+memset(&saddr, 0, sizeof(saddr));
+saddr.sin_family = AF_INET;
+
+memcpy((char *) &saddr.sin_addr.s_addr, h->h_addr_list[0], h->h_length);
+saddr.sin_port = htons(port);
+
+
+
+if (connect(sockfd, (struct sockaddr *) &saddr, sizeof(saddr)) < 0) {
+        printf("Cannot connect, error is %d\n", errno);
+}
+else{
+	int fl = fcntl(sockfd, F_GETFL, 0);
+	      fl |= O_NONBLOCK;
+	      fcntl(sockfd, F_SETFL, fl);
+
+	printf("Connect!\n");// read(sockfd, buffer, sizeof(buffer));
+	while(1){
+
+		while(recv(sockfd, buffer, sizeof(buffer), 0)>0){
+			printf("%s ", "Server: ");
+			printf("%s\n", buffer);
+		}
+		// free(buffer);
+		memset(buffer,0,sizeof(buffer));
+		printf("%s ", "Client: ");
+		scanf("%s", buffer);
+		// int i = 0;
+		// while (1) {
+		//     scanf("%c", &buffer[i]);
+		//     if (buffer[i] == '\n') {
+		//       break;
+		//     }
+		//     else {
+		//       i++;
+		//     }
+		//   }
+		send(sockfd, buffer, sizeof(buffer), 0);
+
+	}
+	close(sockfd);
+}
+}
---
+++ PracticalWork9/server.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <unistd.h> 
+#include <sys/socket.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/select.h>
+
+#define MAX_CLIENT 100
+
+int main(){
+
+	int sockfd, clen, clientfd;
+	struct sockaddr_in saddr, caddr;
+	unsigned short port = 8785;
+	char buffer[100] = "testing ";
+
+	if ((sockfd=socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		printf("Error creating socket \n");
+	}
+	else{
+		printf("Socket!\n");
+	}
+
+	setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &(int){ 1 }, sizeof(int));
+
+	int fl = fcntl(sockfd, F_GETFL, 0);
+	      fl |= O_NONBLOCK;
+	      fcntl(sockfd, F_SETFL, fl);
+
+	memset(&saddr, 0, sizeof(saddr)); 
+	saddr.sin_family = AF_INET; 
+	saddr.sin_addr.s_addr = htonl(INADDR_ANY); 
+	saddr.sin_port = htons(port);
+
+
+
+	if ((bind(sockfd, (struct sockaddr *) &saddr, sizeof(saddr)) < 0)) { 
+		printf("Error binding \n");
+	}
+	else{
+		printf("Bind!\n");
+	}
+
+	if (listen(sockfd, 5) < 0) { 
+		printf("Error listening \n");
+	}
+	else{
+		printf("Listen!\n");
+	}
+
+	clen=sizeof(caddr);
+	// clientfd = accept(sockfd, (struct sockaddr *)&caddr, (socklen_t *)&clen);
+
+	int clientfds[100]; // list of connected clients, >0 if valid
+	memset(clientfds, 0, sizeof(clientfds)); // clear the list
+
+	while(1){
+		fd_set set; // declaration of the set
+		FD_ZERO(&set); //clear the set
+		FD_SET(sockfd, &set);
+		int maxfd = sockfd;
+		for (int i = 0; i < 100; i++) { // add connected client sockets to set
+			if (clientfds[i] > 0) FD_SET(clientfds[i], &set);
+			if (clientfds[i] > maxfd) maxfd = clientfds[i]; 
+		}
+		// poll and wait, blocked indefinitely
+		select(maxfd+1, &set, NULL, NULL, NULL);
+
+		// a «listening» socket?
+		if (FD_ISSET(sockfd, &set)) {
+		clientfd = accept(sockfd, (struct sockaddr *) &saddr, (socklen_t *)&clen);
+		    // make it nonblocking
+		    fl = fcntl(clientfd, F_GETFL, 0);
+		    fl |= O_NONBLOCK;
+		    fcntl(clientfd, F_SETFL, fl);
+		    // add it to the clientfds array
+			for (int i = 0; i < MAX_CLIENT; i++) { 
+				if (clientfds[i] == 0) {
+					if (clientfds[i] == 0) {
+						clientfds[i] = clientfd;
+						break; 
+					}
+				}
+			}
+		}
+
+		// is that data from a previously-connect client?
+		for (int i = 0; i < MAX_CLIENT; i++) {
+			if (clientfds[i] > 0 && FD_ISSET(clientfds[i], &set)) {
+				if (read(clientfds[i], buffer, sizeof(buffer)) > 0) {
+					printf("client %d says: %s\nserver>", clientfds[i], buffer);
+				}
+				else {
+			            // some error. remove it from the "active" fd array
+			            printf("client %d has disconnected.\n", clientfds[i]);
+			            clientfds[i] = 0;
+			    }
+			} 
+		}
+
+	}
+	
+	return 0;
+}
